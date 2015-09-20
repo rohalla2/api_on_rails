cc31ba9594fa7fb83dd8cf4bc372b151b70c0333
Jeff Rohalla <jeffrohalla@gmail.com>
2015-09-19 22:29:05 -0500

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
   # api definition
-  namespace :api, defaults: { format: :json },
-  constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1,
-    constraints: ApiConstraints.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
